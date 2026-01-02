c3167c94f939d2facc80158a4b7f16e5e46a3c9a
Ammar Azzan <22comp1048@isik.edu.tr>
2026-01-02 21:47:37 +0300

    satisfying requirement change three: print even numbers only

--- hwk4/RangePrinter.java
+++ hwk4/RangePrinter.java
@@ -1,23 +1,27 @@
 import java.util.Scanner;
 
 public class RangePrinter {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter first number: ");
-        int a = scanner.nextInt();
+        System.out.print("Enter the first number: ");
+        int start = scanner.nextInt();
 
-        System.out.print("Enter second number: ");
-        int b = scanner.nextInt();
+        System.out.print("Enter the second number: ");
+        int end = scanner.nextInt();
 
-        int start = Math.min(a, b);
-        int end = Math.max(a, b);
-
-        for (int i = start + 1; i < end; i++) {
-            System.out.println(i);
-        }
+        printRange(start, end);
 
         scanner.close();
     }
+
+public static void printRange(int start, int end) {
+    for (int i = start + 1; i < end; i++) {
+        if (i % 2 == 0) {
+            System.out.println(i);
+        }
+    }
 }
 
+}
